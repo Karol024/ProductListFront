5c63ead227bbfbf04fb22efa56ba384394e81eb7
karl <karol010k@gmail.com>
2022-07-15 01:55:01 +0200

    Added component ProductView

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import {ProductView} from "./components/ProductView/ProductView";
 
 
 
 export const App = () => {
     return(
          <>
-
+             <ProductView/>
           </>
 
     );

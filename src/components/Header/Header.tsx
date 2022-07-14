5446f24257d2a7b1c2308d4499b9391af30f2e16
karl <karol010k@gmail.com>
2022-07-15 01:13:00 +0200

    Addded Material nad item , item2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+import {Item} from "../commons/item";
 
 
 export const Header = () => {
 
     return(
-
+        <Item>
          <h1>Lista Produktów</h1>
-
+        </Item>
 
     );
 };
---
+++ src/components/commons/item2.tsx
@@ -0,0 +1,8 @@
+import {styled} from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+export const Item2 = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    color: theme.palette.text.secondary,
+}));
